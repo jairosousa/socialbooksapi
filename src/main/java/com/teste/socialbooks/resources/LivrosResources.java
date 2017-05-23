46dc5497a030d696bb2103c1a29be796b1b8d54a
labes <jaironsousa@gmail.com>
2017-05-23 13:43:43 -0300

    Implementado Delete e Update

--- src/main/java/com/teste/socialbooks/resources/LivrosResources.java
+++ src/main/java/com/teste/socialbooks/resources/LivrosResources.java
@@ -3,8 +3,11 @@ package com.teste.socialbooks.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,20 +18,36 @@ import com.teste.socialbooks.repository.LivrosRepository;
 @RestController
 @RequestMapping("/livros")
 public class LivrosResources {
-	
+
 	@Autowired
 	private LivrosRepository livrosRepository;
 
 	@GetMapping
 	public List<Livro> listar() {
-		
+
 		return livrosRepository.findAll();
 	}
-	
+
 	@PostMapping
 	public void salvar(@RequestBody Livro livro) {
 		livrosRepository.save(livro);
-		
+
+	}
+	
+	@PutMapping("/{id}")
+	public void atualizar(@RequestBody Livro livro, @PathVariable Long id){
+		livro.setId(id);
+		livrosRepository.save(livro);
+	}
+
+	@GetMapping("/{id}")
+	public Livro buscar(@PathVariable("id") Long id) {
+		return livrosRepository.findOne(id);
+	}
+
+	@DeleteMapping("/{id}")
+	public void deletar(@PathVariable("id") Long id) {
+		livrosRepository.delete(id);
 	}
 
 }
